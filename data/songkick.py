92e1c97e38a85493361339a08d8545faf69f944e
Ilja <iljapanic@gmail.com>
2018-03-25 20:11:46 +0100

    extract all interesting values from json to python variables/objects

--- data/songkick.py
+++ data/songkick.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pandas as pd
 import random
 import requests
 
@@ -21,18 +22,61 @@ def getGigs(metro_area_code, min_date, max_date):
   key = getSongkickKey()
   url = 'http://api.songkick.com/api/3.0/metro_areas/'+area+'/calendar.json?min_date='+min_date+'&max_date='+max_date+'&apikey='+key
 
-  print(url)
-
   response = requests.get(url)
   data = json.loads(response.text)
   results = data['resultsPage']['results']['event']
+
   for event in results:
+
+    # event meta
     event_id = event['id']
     event_type = event['type']
     event_url = event['uri']
-    popularity = event['popularity']
-    name = event['displayName']
+    event_popularity = event['popularity']
+    event_name = event['displayName']
 
-    print(event['displayName'])
+    # time
+    start_datetime = event['start']['datetime']
+    start_date = event['start']['date']
+    start_time = event['start']['time']
+
+    # generic location
+    location_lng = event['location']['lng']
+    location_lat = event['location']['lat']
+
+    # venue info
+    venue_id = event['venue']['id']
+    venue_name = event['venue']['displayName']
+    venue_lng = event['venue']['lng']
+    venue_lat = event['venue']['lat']
+
+    artist_list = []
+
+    # artist(s)
+    for artist in event['performance']:
+      artist_name = artist['displayName']
+      artist_billing_index = artist['billingIndex']
+      artist_billing = artist['billing']
+      artist_id = artist['artist']['id']
+      artist_url = artist['artist']['uri']
+      artist_mbid = []
+
+      for identifier in artist['artist']['identifier']:
+        mbid = identifier['mbid']
+        artist_mbid.append(mbid)
+
+      artist_object = {
+        'mbid': artist_mbid,
+        'id': artist_id,
+        'name': artist_name,
+        'songkick_url': artist_url,
+        'billing_index': artist_billing_index,
+        'artist_billing': artist_billing
+      }
+
+      artist_list.append(artist_object)
+
+    # ger everything ready for database query
+
 
 
