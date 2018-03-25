1f141a6c104d8494694432289b1b3393fabd57a0
Ilja <iljapanic@gmail.com>
2018-03-25 13:24:47 +0100

    add SongKick API key randomizer

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # settings.py
 import os
+import random
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
 env_path = Path('.') / '.env'
@@ -13,3 +14,16 @@ DB_NAME=os.getenv('DB_NAME')
 DB_USER=os.getenv('DB_USER')
 DB_PASS=os.getenv('DB_PASS')
 DB_COLL=os.getenv('DB_COLL')
+
+# SongKick API
+songkick_keys = ([
+  os.getenv('SK_API_KEY_1'),
+  os.getenv('SK_API_KEY_2')
+])
+
+# get random SongKick API key function
+def getSongkickKey():
+  key = random.choice (songkick_keys)
+  print('using Songkick key: ' + key)
+  return key
+
