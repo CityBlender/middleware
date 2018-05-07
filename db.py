93820dd2960e07ec4a0726f9a4ba0b6681365d00
Ilja <iljapanic@gmail.com>
2018-05-07 16:54:04 +0100

    add console status updates for inserting events into database

--- db.py
+++ db.py
@@ -29,10 +29,16 @@ def eventExists(new_event_id, db_collection):
 
 # insert events into database
 def dbInsertEvents(events, db_collection):
+  events_inserted = 0;
+  events_skipped = 0;
   for event in events:
     event_id = event['event_id']
     if eventExists(event_id, db_collection):
+      events_skipped = events_skipped + 1
       pass
     else:
+      events_inserted = events_inserted + 1;
       db_collection.insert(event)
+  pprint('Skipped ' str(events_skipped) + ' duplicate events when inseting into database')
+  pprint('Successfully inserted ' str(events_inserted) + ' events into database')
 
