40684b1cfc45c5c09400459e5d8c1a3ad2b41eb7
Ilja <iljapanic@gmail.com>
2018-05-08 20:34:17 +0100

    remove testing clutter

--- fetch.py
+++ fetch.py
@@ -21,24 +21,3 @@ db_london_artist = db_london['artists']
 
 # insert into database
 db.dbInsertEvents(london_area, db_london_events)
-
-# last.getLastById('b6b8a637-e4f1-4d30-b400-2116e9182630')
-
-# last.getArtistTopTracks(mbid='cbc9199f-944b-42e9-a945-627c9fc0ba6e')
-
-# pprint(last.getArtistTopTags(mbid='cbc9199f-944b-42e9-a945-627c9fc0ba6e'))
-
-# final_last = last.returnArtistObject(mbid='cbc9199f-944b-42e9-a945-627c9fc0ba6e')
-# data.dumpJson('final-last-fm-test.json', final_last, './temp/')
-# last.getArtistTopTracks(search='Streetlight Manifesto')
-# data.dumpJson('spotify-test-blockhead-final.json', spotify.returnArtistObject('Blockhead'), './temp/')
-# spotify.getArtistInfo('Blockhead')
-
-# data.dumpJson('last-fm-doors.json', last.returnArtistObject(search='The Doors'), './temp/')
-
-# data.dumpJson('musix-restricted.json', musix.getTrackLyrics('96734374-9968-40e2-ae97-a7632793dd82'), './temp/' )
-# data.dumpJson('musix-instrumental.json', musix.getTrackLyrics('d5a1ad42-898d-4d5b-922c-5891b6c73f5a'), './temp/' )
-# data.dumpJson('musix-lyrics.json', musix.getTrackLyrics('00bde944-7562-446f-ad0f-3d4bdc86b69f'), './temp/' )
-# data.dumpJson('drake.json', last.returnArtistObject(search='Drake'), './temp/' )
-# data.dumpJson('test-artist-object.json', artist.getArtistObject(mbid='cbc9199f-944b-42e9-a945-627c9fc0ba6e'), './temp/')
-# data.dumpJson('test-artist-object-with-search.json', artist.getArtistObject(search='Streetlight Manifesto'), './temp/')
